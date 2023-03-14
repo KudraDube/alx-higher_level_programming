d304b37f791cfff9c1a136d961c17e82eb0f99c5
KudraDube <KudraDube@gmail.com>
2023-03-14 09:09:18 +0000

    0x13-javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.wi
+      this.height = h;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
+dth = w;
