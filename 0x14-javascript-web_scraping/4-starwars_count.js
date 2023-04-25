8c39b540af5e9ee221f9c660fed56d3ce76df493
KudraDube <KudraDube@gmail.com>
2023-04-25 11:29:13 +0000

    0x14 javascript update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,38 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
-
-const apiEndpoint = process.argv[2];
-const characterId = process.argv[3];
-
-request(apiEndpoint, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode === 200) {
-    let movies = JSON.parse(body).results;
-    let countedCharacters = new Set();
-    let count = 0;
-    for (let movieIndex in movies) {
-      let movie = movies[movieIndex];
-      if (!movie.characters) {
-        console.warn(`Warning: movie ${movie.title} is missing characters property`);
-        continue;
-      }
-      for (let characterUrlIndex in movie.characters) {
-        let characterUrl = movie.characters[characterUrlIndex];
-        let characterIdMatch = characterUrl.match(/\/(\d+)\/$/);
-        if (!characterIdMatch) {
-          console.warn(`Warning: invalid character URL ${characterUrl}`);
-          continue;
-        }
-        let id = parseInt(characterIdMatch[1]);
-        if (id === characterId && !countedCharacters.has(id)) {
-          count++;
-          countedCharacters.add(id);
-        }
-      }
-    }
-    console.log(count);
-  } else {
-    console.log(`Error: API returned status code ${response.statusCode}`);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
+
